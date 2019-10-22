c00b11cdc5a051fa07591eee6420d7b1c67d78ba
Mahdi Chaari <mahdi-chaari@hotmail.com>
2019-10-22 17:18:28 -0400

    Prepared the spine for user profile management.

--- src/views/AccountUpdation.vue
+++ src/views/AccountUpdation.vue
@@ -25,6 +25,62 @@
                         <v-layout justify-center>
                             <span>Profile</span>
                         </v-layout>
+                        <v-form v-if='menuPosition === "editProfile"' v-model="valid">
+                            <v-container>
+                                <v-row>
+                                    <v-col md="5">
+                                        <v-text-field required v-model="firstname" :rules="nameRules" label="First name"/>
+                                    </v-col>
+                                    <v-col md="5">
+                                        <v-text-field required v-model="lastname" :rules="nameRules" label="Last name"/>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col md="6">
+                                        <v-text-field required v-model="email" :rules="emailRules" label="E-mail"/>
+                                    </v-col>
+                                    <v-col md="4">
+                                        <v-text-field v-model="phoneNumber" :rules="phoneNumberRules" label="Phone Number"/>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col md="10">
+                                        <v-text-field v-model="shippingAddress" :rules="shippingAddressRules" label="Shipping Address"/>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col>
+                                        <v-btn :disabled="!valid" class="mt-5" @click="validate()">
+                                            Save
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-form>
+                        <v-form v-if='menuPosition === "managePassword"' v-model="valid">
+                            <v-container>
+                                <v-row>
+                                    <v-col md="4">
+                                        <v-text-field required label="Old Password"/>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col md="4">
+                                        <v-text-field required :rules="passwordRules" label="New Password"/>
+                                    </v-col>
+                                    <v-col md="4">
+                                        <v-text-field required :rules="passwordRules" label="Confirm Password"/>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col>
+                                        <v-btn :disabled="!valid" class="mt-5" @click="validate()">
+                                            Save
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-form>
                     </v-col>
                 </v-row>
             </v-container>
@@ -45,6 +101,29 @@
                 { icon: 'far fa-user-circle', text: 'Manage password', strVal: 'managePassword' },
                 { icon: 'security', text: 'Security', strVal: 'security' },
                 { icon: 'info', text: 'About', strVal: 'about' }
+            ],
+            valid: false,
+            firstname: "",  //TODO: Default to the registered name
+            lastname: "",
+            nameRules: [
+                v => !!v || "Full name is required",
+                v => v.length <= 26 || "Name must be less than 26 characters",
+            ],
+            email: "",  //TODO: Default to the registered e-mail
+            emailRules: [
+                v => !!v || "E-mail is required",
+                v => /.+@.+/.test(v) || "E-mail must be valid"
+            ],
+            phoneNumber: "",
+            phoneNumberRules: [
+                v => !v || /^(\()?\d{3}(\))?(-|\s)?[2-9]\d{2}(-|\s)\d{4}$/.test(v) || "Phone number must be valid"
+            ],
+            shippingAddress: "",
+            shippingAddressRules: [
+
+            ],
+            passwordRules: [
+
             ]
         }),
         methods: {
