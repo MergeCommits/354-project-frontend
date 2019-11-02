dc06d8d64eac98ecc031fec2411afcdbedc794f1
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2019-11-02 15:00:05 -0400

    password constant removed

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -82,8 +82,6 @@
     import {APICall, RequestType} from "../components/common/API";
 
     const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const PASSWORD_PATTERN = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-
     export default {
         name: "Login",
         mixins: [Utilities],
