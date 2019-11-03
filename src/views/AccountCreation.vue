193a4f3a9607c56a578848833f473029c799ed73
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2019-11-03 15:49:12 -0500

    valid length check

--- src/views/AccountCreation.vue
+++ src/views/AccountCreation.vue
@@ -161,7 +161,7 @@
                 if (this.$refs.form.validate()) {
                     await Requests.headRequest({username: this.username}, "username").then(errors => this.usernameErrors = errors);
                     await Requests.headRequest({email: this.email}, "email").then(errors => this.emailErrors = errors);
-                    if (this.emailErrors.length > 0 && this.usernameErrors.length > 0) {
+                    if (this.emailErrors.length <= 0 && this.usernameErrors.length <= 0) {
                         Requests.postRequest({
                             firstName: this.firstName,
                             lastName: this.lastName,
