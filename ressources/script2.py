f7322c10f89931cc871f64e01e2bb5e1528400ad
gheith <gheith.abinader@gmail.com>
2019-11-24 13:47:36 -0500

    all better

--- ressources/script2.py
+++ ressources/script2.py
@@ -1,5 +1,8 @@
 import json
 import re
+import psycopg2 as pg
+import pandas as pd
+import pandas.io.sql as psql
 
 from pandas.io.json import json_normalize
 
@@ -9,14 +12,40 @@ def slugify(value):
     Normalizes string, converts to lowercase, removes non-alpha characters,
     and converts spaces to hyphens.
     """
-    value = re.sub('[^\w\s-]', '', value).strip().lower()
+    value = re.sub('[^\w\s-(\'s)]', '', value).strip().lower()
     value = re.sub('[-\s]+', '-', value)
     return value
+
+
+# get connected to the database
+connection = pg.connect(dbname="postgres",
+                        user="postgres",
+                        password="vLfmVz@7O51P",
+                        host="dev.354thestars.com",
+                        port="5432")
+
+cats = psql.read_sql("SELECT * FROM section", connection)
+cats_to_add = pd.DataFrame(columns=cats.columns)
 
 jsonStr = open("json_dat_sample.json", "r", encoding="utf-8").read()
 raw_json = json.loads(jsonStr)
 
-cat3 = json_normalize(raw_json['categories'], ['subCategories', 'listings', 'imageUrls'],
-                       [['subCategories', 'label'], ['subCategories', 'listings', 'label']])
+cats_to_add_dat = json_normalize(raw_json['categories']).drop(columns='subCategories')
+cats_exists = i in cats[['name']].values
+
+print(cats_exists)
+print(cats_to_add_dat)
+
+
+# todo replace with sequence and sequence.next()
+seq = (cats.iloc[-1,:]['id'])
+for i in
+
+connection.close()
+
+
+#
+# cat3 = json_normalize(raw_json['categories'], ['subCategories', 'listings', 'imageUrls'],
+#                        [['subCategories', 'label'], ['subCategories', 'listings', 'label']])
 
 
