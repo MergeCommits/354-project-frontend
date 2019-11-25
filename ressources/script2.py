3a8f94f6822987f3f2cec4a80d9229520572c22d
gheith <gheith.abinader@gmail.com>
2019-11-25 10:23:15 -0500

    Done hopefully

--- ressources/script2.py
+++ ressources/script2.py
@@ -68,26 +68,22 @@ with open('SQL_QUERIES.txt', 'w', encoding="utf-8") as f:
                                             'permalink': slugify(v['sub_label']), 'icon': v['sub_imageUrl']}
                                            , ignore_index=True)
     cat = pd.concat([cats2, cats2_to_add])
-    # ['sub_label', 'sub_price', 'sub_imageUrls', 'sub_description', 'sub__references', 'sub_sellerInfo.name',
-    #  'sub_sellerInfo._system_Mail',  'subCategories.label', 'id', 'section_id', 'name', 'description', 'permalink'
-    # ['id', 'name', 'description', 'quantity', 'aggragated_review', 'number_of_review', 'category_id', 'user_id',
-    #  'tax_id', 'date_added', 'permalink', 'specifications', 'photos', 'brand_id', 'condition']
+
     sql = "INSERT INTO product (id, name, description, quantity, category_id, permalink, photos, user_id, brand_id, date_added)" \
           " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     seq=0
     pdTry = pd.merge(prods, cat, left_on='subCategories.label', right_on='name')
     for k, v in pdTry.iterrows():
         seq += 1
-        desc = v['sub_description'][:999] if len(v['sub_description']) > 1000 else v['sub_description']
-        picD = {i:v['sub_imageUrls'][i] for i in range(len(v['sub_imageUrls']))}
+        desc = v['sub_description'].replace('\r', '')
+        desc = desc.replace('\n', '\\n')
+        desc = desc[:999] if len(desc) > 1000 else desc
+        picD = {i: v['sub_imageUrls'][i] for i in range(len(v['sub_imageUrls']))}
         cursor.execute(sql, (seq, v['sub_label'], desc, random.randint(1,5), v['id'],
                              slugify(v['sub_label']), json.dumps(picD), 1, 1, '2019-06-01'))
         f.write(sql % (seq, v['sub_label'], desc, random.randint(1,5), v['id'],
                                      slugify(v['sub_label']), json.dumps(picD), 1, 1, '2013-06-01') + '\n')
 
-print(cat.columns)
-print(prods.columns)
-print()
 # connection.commit()
 connection.close()
 
