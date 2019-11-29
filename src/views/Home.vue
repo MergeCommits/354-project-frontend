eb4ffea447409ab12c089562e5ac39932b463321
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2019-11-29 13:56:29 -0500

    UI modifications

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,19 +3,20 @@
         <titlebar/>
         <v-container fluid>
             <v-row style="margin-top: -13px;">
-                <v-layout justify-center pt-5>
+                <v-layout justify-center pt-5 v-if="categories.length > 0">
                     <v-carousel cycle height="300" style="max-width: 70%; border-radius: 10px!important;"
                                 hide-delimiter-background :show-arrows="false">
                         <v-carousel-item v-for="(category, i) in categories.slice(0,10)" :key="i"
                                          :src="category.imageUrl" interval="2s">
-                            <v-card height="300" width="30%" color="rgba(255,255,255,0.51)" style="padding: 10px">
+                            <v-card flat height="300" width="45%"
+                                    style="padding: 10px; background: linear-gradient(270deg, rgba(255,255,255,0), #00838F 60%) no-repeat;">
                                 <v-container>
                                     <v-row>
                                         <v-layout>
-                                            <v-sheet width="100%" height="140%" elevation="4" color="teal">
-                                                <span style="color: black; font-size: 30px!important;"
-                                                      class="title font-weight-regular">{{category.name}}</span>
-                                            </v-sheet>
+                                            <v-layout justify-left pt-3 style="margin-left: 20px">
+                                                    <span style="color: white; font-size: 30px!important;"
+                                                          class="title font-weight-regular">{{category.name}}</span>
+                                            </v-layout>
                                         </v-layout>
                                     </v-row>
                                     <v-row>
@@ -23,9 +24,9 @@
                                             <span style="color: black" class="title font-weight-regular">{{category.title}}</span>
                                         </v-layout>
                                     </v-row>
-                                    <v-row style="margin-top: 60px;">
-                                        <v-layout justify-center>
-                                            <v-btn outlined color="black"
+                                    <v-row style="margin-top: 20px;">
+                                        <v-layout justify-left style="margin-left: 5%">
+                                            <v-btn outlined color="white"
                                                    @click="selectedCategory = category; $router.push({ name: 'search'})">
                                                 Shop now
                                                 <v-icon style="margin-left: 5px; font-size: 20px!important;">
@@ -39,12 +40,17 @@
                         </v-carousel-item>
                     </v-carousel>
                 </v-layout>
+                <v-layout justify-center pt-5 v-else>
+                    <v-progress-circular size="30" indeterminate :color="PRIMARY_COLOR"></v-progress-circular>
+                </v-layout>
             </v-row>
             <v-row>
                 <v-layout justify-center>
                     <v-card flat height="450px" color="transparent" width="85%">
                         <v-layout justify-center pt-3>
-                            <v-card-title>Popular items</v-card-title>
+                            <v-card-title>Popular items
+                                <v-icon style="margin-left: 10px" :color="ACCENT_COLOR">fab fa-hotjar</v-icon>
+                            </v-card-title>
                         </v-layout>
                         <v-layout justify-center wrap pt-5 style="height: 400px!important;">
                             <!-- TODO: Make this use category.name for key binding when we eventually get unique names for them. -->
@@ -53,13 +59,18 @@
                                     max-height="100px"
                                     max-width="190px" hover flat outlined
                                     @click="selectedCategory = category; $router.push({ name: 'search'})"
-                                    style="margin-right: 3%; margin-top: -10px; border-radius: 10px; "
+                                    style="margin-right: 3%; margin-top: -10px; border-radius: 10px;"
                                     class="animated fadeIn" v-if="categories.length > 0">
                                 <v-img class="white--text" height="150px" :src="category.imageUrl"
                                        lazy-src="https://picsum.photos/id/11/10/6">
-                                    <v-card-title class="align-end fill-height font-weight-regular"
-                                                  style="font-size: 18px!important;">Top 10 {{category.name}}
-                                    </v-card-title>
+                                    <v-card flat max-height="27px"
+                                            style="overflow: hidden; border-bottom-right-radius: 0; border-bottom-left-radius: 0">
+                                        <v-layout justify-center>
+                                        <span class="align-end fill-height font-weight-regular"
+                                              style="color:#424242; font-size: 15px!important;">
+                                           {{category.name}}</span>
+                                        </v-layout>
+                                    </v-card>
                                 </v-img>
                             </v-card>
                             <v-progress-circular v-else size="30" indeterminate
